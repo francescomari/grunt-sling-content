b59b9b566780005001d4da8617b562c03f8df88f
Francesco Mari <mari.francesco@gmail.com>
2013-08-29 17:58:45 +0200

    Remove unused imports

--- tasks/sling-import.js
+++ tasks/sling-import.js
@@ -1,9 +1,3 @@
-var util = require("util");
-var path = require("path");
-var async = require("async");
-
-var servlet = require("./lib/servlet");
-
 var tasks = require("./lib/tasks");
 
 module.exports = function (grunt) {
